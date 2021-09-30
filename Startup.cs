f55a2cc0dee5056f71107a8507b5fcb3de318ec7
vihernandes <vinicius.hernandes@indt.org.br>
2021-09-30 20:12:55 -0300

    added cors

--- Startup.cs
+++ Startup.cs
@@ -47,11 +47,12 @@ namespace prova_dsv_back
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "prova_dsv_back v1"));
             }
 
-            app.UseHttpsRedirection();
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
